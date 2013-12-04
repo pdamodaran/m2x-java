620fbd0d9cb7c3ecc29a13ba5d37be8d401a711d
pkoshaleu <pavel.koshaleu@gmail.com>
2013-12-04 19:53:19 +0300

    Add list methods to Feed resource

---
+++ src/main/java/com/att/m2x/client/api/feed/FeedListResponse.java
@@ -0,0 +1,15 @@
+package com.att.m2x.client.api.feed;
+
+import java.util.List;
+
+
+public class FeedListResponse {
+
+    private List<CommonFeed> feeds;
+
+    public List<CommonFeed> getFeeds() {
+        return feeds;
+    }
+
+}
+
--- src/main/java/com/att/m2x/client/resource/FeedResource.java
+++ src/main/java/com/att/m2x/client/resource/FeedResource.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.HttpClient;
 
 import com.att.m2x.client.api.feed.CommonFeed;
+import com.att.m2x.client.api.feed.FeedListResponse;
 import com.att.m2x.client.internal.ExecutableResource;
 import com.att.m2x.client.resource.feed.FeedSubResource;
 
@@ -14,7 +15,22 @@ public class FeedResource extends ExecutableResource {
         super(path, client, mapper);
     }
 
-    //TODO: PK,03/12: list
+    public FeedListResponse list() {
+        return execute(prepare().get()).status(200).as(FeedListResponse.class);
+    }
+
+    public FeedListResponse list(String query) {
+        return execute(prepare().get().params("q", query)).status(200).as(FeedListResponse.class);
+    }
+
+    public FeedListResponse listByType(String query) {
+        return execute(prepare().get().params("type", query)).status(200).as(FeedListResponse.class);
+    }
+
+    //TODO: PK,04/12, replace type with enum
+    public FeedListResponse list(String query, String type) {
+        return execute(prepare().get().params("q", query, "type", type)).status(200).as(FeedListResponse.class);
+    }
 
     public CommonFeed get(String id) {
         return execute(prepare().get(id)).status(200).as(CommonFeed.class);
